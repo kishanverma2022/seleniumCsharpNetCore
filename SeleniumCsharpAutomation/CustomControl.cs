257af31e986ddcc937e5aafb8adfa6b39433f075
kishanverma2022 <kishanverma.blr@gmail.com>
2022-02-06 17:13:02 +0530

    Initial Commit until Combo controls
    Initial Commit until Combo controls

---
+++ SeleniumCsharpAutomation/CustomControl.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCsharpAutomation
+{
+    public class CustomControl:DriverHelper
+    {
+        public static void ComboBox(string controlName, string value)
+        {
+            IWebElement combobox = driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            combobox.Clear();
+            combobox.SendKeys(value);
+            driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text(),'{value}']")).Click();
+        }
+    }
+}
---
+++ SeleniumCsharpAutomation/UnitTest1.cs
@@ -0,0 +1,93 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumCsharpAutomation
+{
+    [TestFixture]
+    public class Tests: DriverHelper
+    {
+     
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            driver = new ChromeDriver();
+        }
+
+        // [Test]
+        //public void Test1()
+        //{
+        //    driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        //    driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+        //    Assert.Pass();
+        //}
+
+        //[Test]
+        //public void Test2()
+        //{
+
+        //    driver.Navigate().GoToUrl("https://www.spicejet.com/");
+        //     driver.FindElement(By.LinkText("round trip")).Click();
+        //    bool sel= driver.FindElement(By.LinkText("round trip")).Selected;
+        //    if (sel)
+        //    {
+        //        Console.WriteLine("Test Pass");
+        //        Assert.Pass();
+        //    }
+        //    else
+        //        Console.WriteLine("Fail");
+
+        //}
+
+        //[Test]
+        //public void AjaxControlText()
+        //{
+        //    driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        //    driver.FindElement(By.XPath("//Input[@class='ctl00$ContentPlaceHolder1$ChildMeal1']")).Click();
+        //    //driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("to");
+        //    //string resultbox=driver.FindElement(By.Id("ContentPlaceHolder1_Meal-dropmenu")).Text;
+        //    //Console.WriteLine(resultbox);
+        //    driver.Quit();
+
+
+        //}
+
+        //[Test]
+        //public void ddrDownProgram()
+        //{
+        //    driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        //    SelectElement select = new SelectElement(driver.FindElement(By.Id("ContentPlaceHolder1_Add1-awed")));
+        //    System.Threading.Thread.Sleep(5000);
+        //    select.SelectByText("Broccoli");
+        //    System.Threading.Thread.Sleep(5000);
+        //    driver.Quit();
+
+
+        //}
+        //[Test]
+        //public void ddrDownProgram()
+        //{
+        //    driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+        //    System.Threading.Thread.Sleep(3000);
+        //    driver.FindElement(By.XPath("//div[contains(text(),'Celery')]")).Click();
+            
+        //    System.Threading.Thread.Sleep(5000);
+        //    driver.Quit();
+        //}
+
+        [Test]
+        public void comboboxProgram()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            System.Threading.Thread.Sleep(3000);
+            
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almonds");
+            System.Threading.Thread.Sleep(5000);
+            driver.Quit();
+        }
+
+    }
+}
